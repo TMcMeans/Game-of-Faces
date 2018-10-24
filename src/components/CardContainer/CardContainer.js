49ebbfd8d7f805841bcfd75e35df010e91a1547b
Tanjie McMeans <tanjmcmeans01@gmail.com>
2018-10-24 16:16:48 -0600

    Add CardContainer Component

---
+++ src/components/CardContainer/CardContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './CardContainer.css';
+
+const CardContainer = () => {
+  return (
+    <div>
+      <h1>I am a CardContainer Component</h1>
+    </div> 
+  )
+}
+
+export default CardContainer;
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 import './Sidebar.css';
+import FilterList from '../FilterList/FilterList.js';
 
 const Sidebar = () => {
   return (
     <div>
-      
+     <h1>I am a Sidebar Component</h1>
+     <FilterList />
     </div>
   )
 }
 
 export default Sidebar; 
-
-//Add CardContainer and FilterContainer to Sidebar 
